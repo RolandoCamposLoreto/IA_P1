684a17daee789fd3ff5a8ff550b2d022ce936779
Rolando <143459727+RolandoCamposLoreto@users.noreply.github.com>
2025-03-18 03:32:39 +0000

    003_CaracterEscape.py

---
+++ 003_CaracterEscape.py
@@ -0,0 +1,6 @@
+print("La vida te prepara para volar.")
+print() #La inovación esta en que no se le entrega un argumento a la funcion, generando una nueva linea
+print("A pesar de no saber como.")
+print("La vida te prepara\n para volar.") #El \n (carácter de escape) agrega una newline
+print()
+print("A pesar de\n no saber como.")
